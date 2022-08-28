3c3aab2ecc1bd4ca1b35b31004701fdab242419f
BrendenSmart <brenden.smart24@gmail.com>
2022-08-28 13:35:33 -0500

    add basic library creation

--- src/com/bsmart/javafreetype/FreeType.java
+++ src/com/bsmart/javafreetype/FreeType.java
@@ -1,16 +1,3 @@
-package com.bsmart.javafreetype;
-
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.lang.foreign.*;
-import java.lang.invoke.MethodHandle;
-import java.nio.file.FileAlreadyExistsException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-
-import static java.lang.foreign.ValueLayout.*;
-
-
 /*
  * MIT License
  *
@@ -35,6 +22,18 @@ import static java.lang.foreign.ValueLayout.*;
  *
  */
 
+package com.bsmart.javafreetype;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.lang.foreign.*;
+import java.lang.invoke.MethodHandle;
+import java.nio.file.FileAlreadyExistsException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import static java.lang.foreign.ValueLayout.*;
+
 /**
  * @author Brenden Smart
  */
@@ -46,7 +45,7 @@ public final class FreeType {
 
     private static final MemorySession MEMORY = MemorySession.openShared();
 
-    private static MethodHandle FT_Init_FreeType, FT_Done_FreeType;
+    private static MethodHandle CreateLibrary, DestroyLibrary, CreateFace, GetNumFaceGlyphs, GetFaceAscender;
 
     private FreeType() {}
 
@@ -55,15 +54,23 @@ public final class FreeType {
         initIDs();
     }
 
+    @SuppressWarnings("SpellCheckingInspection")
     private static void loadLibrary() {
-        final String libraryResource = FreeType.class.getResource("/natives/windows/freetype.dll").getPath().replace("%20", " ");
+        final String libraryResource = FreeType.class.getResource("/natives/windows/javafreetype.dll").getPath().replace("%20", " ");
 
 
         final String userHome = System.getProperty("user.home");
 
-        Path folder = Path.of(userHome + "/java-freetype");
+        Path folder = Path.of(userHome + "/javafreetype");
 
-        Path tempLibrary = Path.of(userHome + "/java-freetype/freetype.dll");
+        Path tempLibrary = Path.of(userHome + "/javafreetype/javafreetype.dll");
+
+        try {
+            Files.deleteIfExists(tempLibrary);
+            Files.deleteIfExists(folder);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
         try {
             Files.createDirectory(folder);
@@ -81,12 +88,58 @@ public final class FreeType {
         folder.toFile().deleteOnExit();
         tempLibrary.toFile().deleteOnExit();
 
-        freetype = SymbolLookup.libraryLookup(tempLibrary, MEMORY);
+        freetype = SymbolLookup.libraryLookup(tempLibrary, MemorySession.openShared());
         linker = Linker.nativeLinker();
     }
 
     private static void initIDs() {
+        CreateLibrary = linker.downcallHandle(freetype.lookup("CreateLibrary").orElseThrow(), FunctionDescriptor.of(JAVA_LONG));
+        DestroyLibrary = linker.downcallHandle(freetype.lookup("DestroyLibrary").orElseThrow(), FunctionDescriptor.of(JAVA_INT, JAVA_LONG));
+        CreateFace = linker.downcallHandle(freetype.lookup("CreateFace").orElseThrow(), FunctionDescriptor.of(JAVA_LONG, JAVA_LONG, ADDRESS, JAVA_INT));
+        GetNumFaceGlyphs = linker.downcallHandle(freetype.lookup("GetNumFaceGlyphs").orElseThrow(), FunctionDescriptor.of(JAVA_INT, JAVA_LONG));
+        GetFaceAscender = linker.downcallHandle(freetype.lookup("GetFaceAscender").orElseThrow(), FunctionDescriptor.of(JAVA_SHORT, JAVA_LONG));
+    }
 
+
+    public static long CreateLibrary() {
+        try {
+            return (long) CreateLibrary.invokeExact();
+        } catch (Throwable e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static int DestroyLibrary(long library) {
+        try {
+            return (int) DestroyLibrary.invokeExact(library);
+        } catch (Throwable e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static long CreateFace(long library, String filepath, int index) {
+        MemorySegment path = MEMORY.allocateUtf8String(filepath);
+        try {
+            return (long) CreateFace.invokeExact(library, (Addressable) path, index);
+        } catch (Throwable e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static int GetNumFaceGlyphs(long face) {
+        try {
+            return (int) GetNumFaceGlyphs.invokeExact(face);
+        } catch (Throwable e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static short GetFaceAscender(long face) {
+        try {
+            return (short) GetFaceAscender.invokeExact(face);
+        } catch (Throwable e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
